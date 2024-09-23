2b4b41c3b25ba2fabff1e2d08a88baaaf6b833d7
web223dev <ruslanus866@outlook.com>
2024-09-23 17:56:14 +0800

    Compound component pattern

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,32 @@ export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      <Counter
+      {/* <Counter
         iconIncrease='+'
         iconDecrease='-'
         label='My NOT so flexible counter'
         hideLabel={false}
         hideIncrease={false}
         hideDecrease={false}
-      />
+        positionCount='top'
+      /> */}
+
+      <Counter>
+        <Counter.Label>My super flexable counter</Counter.Label>
+        <Counter.Decrease icon='-' />
+        <Counter.Increase icon='+' />
+        <Counter.Count />
+      </Counter>
+
+      <div>
+        <Counter>
+          <Counter.Decrease icon='◀️' />
+          <div>
+            <Counter.Count />
+          </div>
+          <Counter.Increase icon='▶️' />
+        </Counter>
+      </div>
     </div>
   );
 }
